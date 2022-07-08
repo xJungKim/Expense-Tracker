042337b313df3a3ed2c90dfd9495625a7a19df2e
xJungKim <roflcoptart1@gmail.com>
2022-07-08 16:30:21 -0700

    7-8-2022 (2)
    Fixed an error within ChartBar.js that caused the bars to not show up visually as intended.

--- src/Components/Chart/Chart.js
+++ src/Components/Chart/Chart.js
@@ -1,11 +1,36 @@
-import ChartBar from "./ChartBar";
-import "./Chart.css";
+// import ChartBar from "./ChartBar";
+// import "./Chart.css";
 
-function Chart(props) {
-    const dataPointValues = props.dataPoints.map(dataPoint => dataPoint.value);
-    const totalMaximum = Math.max(...dataPointValues);
+// function Chart(props) {
+//     const dataPointValues = props.dataPoints.map(dataPoint => dataPoint.value);
+//     const totalMaximum = Math.max(...dataPointValues);
+//   return (
+//     <div className="chart">
+//       {props.dataPoints.map((dataPoint) => (
+//         <ChartBar
+//           key={dataPoint.label}
+//           value={dataPoint.value}
+//           maxValue={totalMaximum}
+//           label={dataPoint.label}
+//         />
+//       ))}
+//     </div>
+//   );
+// }
+
+// export default Chart;
+
+import React from 'react';
+
+import ChartBar from './ChartBar';
+import './Chart.css';
+
+const Chart = (props) => {
+  const dataPointValues = props.dataPoints.map(dataPoint => dataPoint.value);
+  const totalMaximum = Math.max(...dataPointValues);
+
   return (
-    <div className="chart">
+    <div className='chart'>
       {props.dataPoints.map((dataPoint) => (
         <ChartBar
           key={dataPoint.label}
@@ -16,6 +41,6 @@ function Chart(props) {
       ))}
     </div>
   );
-}
+};
 
 export default Chart;
--- src/Components/Chart/ChartBar.js
+++ src/Components/Chart/ChartBar.js
@@ -4,7 +4,7 @@ function ChartBar(props) {
   let barFillHeight = "0%";
 
   if (props.maxValue > 0) {
-    barFillHeight = Math.round((props.value / props.maxValue) * 100 + "%");
+    barFillHeight = Math.round((props.value / props.maxValue) * 100) + "%";
   }
   return (
     <div className="chart-bar">
